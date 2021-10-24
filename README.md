e9b8c10ad064e5e2b0d5542dfef9aeeba5208f9b
Tsaicming <tsai.cming@gmail.com>
2021-10-24 23:05:49 +0800

    feat: 修改 seeder docs: 修改 readme

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # 餐廳清單作業練習
-## AC 2-3 A5 作業練習
-Node.js、Express、Git、GitHub、JSON、MongoDB綜合應用
+## AC 3 A1 作業練習
+Node.js、Express、Git、GitHub、JSON、MongoDB、使用者註冊及登錄綜合應用
 ## 功能
-* 餐廳清單列表
+* 可依不同使用者建立帳號並記錄餐廳清單列表
+* 可使用 Facebook 登錄
 * 可依**餐廳名稱**或**餐廳類別**搜尋特定餐廳
 * 點選餐廳後可看到餐廳的詳細資訊
 * 可新增、修改、刪除餐廳資訊
 ## 需求
 * Node.js
 * Express
+* MongoDB
+## 使用
+* clone project
+* npm i
+* npm run dev 啟動專案
+* npm run seed 建立種子資料
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,10 +1,61 @@
+// const mongoose = require('mongoose')
+// const Restaurant = require('../restaurant')
+// const restaurantsSeed = require('../seeds/restaurant.json').results
+
+// mongoose.connect('mongodb://localhost/restaurant-list')
+
+// const db = mongoose.connection
+// db.on('error', () => {
+//   console.log('mongodb error!')
+// })
+
+// db.once('open', () => {
+//   console.log('mongodb connected!')
+
+//   for (let i = 0; i < restaurantsSeed.length; i++) {
+//     Restaurant.create({
+//       name: restaurantsSeed[i].name,
+//       name_en: restaurantsSeed[i].name_en,
+//       category: restaurantsSeed[i].category,
+//       image: restaurantsSeed[i].image,
+//       location: restaurantsSeed[i].location,
+//       phone: restaurantsSeed[i].phone,
+//       google_map: restaurantsSeed[i].google_map,
+//       rating: restaurantsSeed[i].rating,
+//       description: restaurantsSeed[i].description
+//     })
+//   }
+
+//   console.log('seeder finished!')
+// })
+
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
+const User = require('../user')
 const restaurantsSeed = require('../seeds/restaurant.json').results
 
-mongoose.connect('mongodb://localhost/restaurant-list')
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurantID: [1, 2, 3]
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurantID: [4, 5, 6]
+  }
+]
 
+mongoose.connect('mongodb://localhost/restaurant-list')
 const db = mongoose.connection
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
@@ -12,19 +63,24 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 
-  for (let i = 0; i < restaurantsSeed.length; i++) {
-    Restaurant.create({
-      name: restaurantsSeed[i].name,
-      name_en: restaurantsSeed[i].name_en,
-      category: restaurantsSeed[i].category,
-      image: restaurantsSeed[i].image,
-      location: restaurantsSeed[i].location,
-      phone: restaurantsSeed[i].phone,
-      google_map: restaurantsSeed[i].google_map,
-      rating: restaurantsSeed[i].rating,
-      description: restaurantsSeed[i].description
+  Promise.all(SEED_USER.map(SEED_USERS =>
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USERS.password, salt))
+      .then(hash => User.create({
+        name: SEED_USERS.name,
+        email: SEED_USERS.email,
+        password: hash
+      }))
+      .then(user => {
+        const restaurants = restaurantsSeed.filter(restaurant => SEED_USERS.restaurantID.includes(restaurant.id))
+        restaurants.forEach(restaurant => { restaurant.userId = user._id })
+        return Restaurant.create(restaurants)
+      })
+  ))
+    .then(() => {
+      console.log('done')
+      process.exit()
     })
-  }
-
-  console.log('seeder finished!')
+    .catch(err => console.log(err))
 })
