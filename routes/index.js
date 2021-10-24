de1463fe4b3a05b039d7f46cd36a66483b66f1b1
Tsaicming <tsai.cming@gmail.com>
2021-10-24 14:04:47 +0800

    feat: 新增 user 路由; 新增 login、register 頁面。 refactor: 重構各頁面 <header> 標籤

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const router = express.Router()     // 載入 express 的路由模組
 
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 router.use('/', home)                     // 當路徑是 '/' ，就執行 './modules/home' 裡的程式
 router.use('/restaurants', restaurants)   // 當路徑是 '/restaurants' ，就執行 './modules/restaurants' 裡的程式
+router.use('/users', users)
 
 module.exports = router
